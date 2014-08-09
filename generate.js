825c13442fce797619e3bb7dca966e113bbe2c94
Daniel <dano.husar@gmail.com>
2014-08-09 22:10:38 +0100

    use download to get all licenses

--- generate.js
+++ generate.js
@@ -1,45 +1,13 @@
 'use strict';
 
-var fs = require('fs');
-var request = require('request');
-var cheerio = require('cheerio');
-var baseUrl = 'http://git.spdx.org/';
-var ignored = ['README.txt', 'Updating the SPDX Licenses.txt'];
+var download = require('download');
 
-var generateLicense = function (name, url) {
-	request(baseUrl + url).on('response', function (res) {
-		var data = '';
-		res.on('data', function (chunk) {
-			data += chunk.toString();
-		});
-
-		res.on('end', function () {
-			fs.writeFile('./licenses/' + name, data, function (err) {
-				if (err) {
-					console.log(err);
-				} else {
-					console.log(name + ' was saved!');
-				}
-			});
-		});
-	});
-};
-
-request(baseUrl + '?p=license-list.git;a=tree').on('response', function (res) {
-	var data = '';
-	res.on('data', function (chunk) {
-		data += chunk.toString();
+download({
+		url: 'http://git.spdx.org/?p=license-list.git;a=snapshot;h=HEAD;sf=tgz',
+		name: 'licenses.tar.gz'
+	}, 'licenses', {
+		extract: true,
+		strip: 1,
+		ext: 'application/x-gzip'
 	});
 
-	res.on('end', function () {
-		var $ = cheerio.load(data);
-		$('.tree tr').each(function (i, e) {
-			var name = $(e).find('a.list').text();
-			var url = $(e).find('td.link a:last-child').attr('href');
-			if (name.match(/\.txt$/) && ignored.indexOf(name) === -1) {
-				name = name.replace(/\.txt$/, '');
-				generateLicense(name, url);
-			}
-		});
-	});
-});
