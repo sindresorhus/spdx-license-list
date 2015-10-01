7e0456df47e5f2558272e4145be23e4a0ef35be1
Sami Lempinen <sami.lempinen@opero.fi>
2015-10-01 21:50:39 +0300

    Add a test for the source URL.

--- test.js
+++ test.js
@@ -4,5 +4,6 @@ var spdxLicenseList = require('./');
 
 test(function (t) {
 	t.assert(spdxLicenseList.MIT.name === 'MIT License');
+  t.assert(spdxLicenseList.MIT.url  === 'http://www.opensource.org/licenses/MIT');
 	t.assert(require('./spdx-full.json').MIT.license.length > 0);
 });
