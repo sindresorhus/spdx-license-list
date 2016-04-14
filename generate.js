54b40c8b4f42e39e337276eb0f459976e3509265
Sebastian Schuberth <sschuberth@gmail.com>
2016-04-14 15:56:49 +0200

    Directly use the offical SPDX JSON files as the source
    This simplifies the code. Also commit the individual JSON files for each
    license as taken form [1] for convenience.
    
    As a side effect, the new implementation fixes an issue where multiple
    URLs would be separated by newlines in the "url" field. Use only the first
    URL instead. Also the issue where the full licence text would contain SPDX
    template markers is fixed.
    
    Fixes #6.
    
    [1] https://spdx.org/licenses/*.json

--- generate.js
+++ generate.js
@@ -1,76 +1,60 @@
 'use strict';
+var d3 = require('d3-queue');
 var fs = require('fs');
-var path = require('path');
-var zlib = require('zlib');
-var tar = require('tar');
 var got = require('got');
-var cv2json = require('convert-json');
-var globby = require('globby');
-var del = require('del');
 
 // config
-var url = 'http://git.spdx.org/?p=license-list.git;a=snapshot;h=HEAD;sf=tgz';
-var folder = 'licenses';
-var ignored = [
-	'README.txt',
-	'Updating the SPDX Licenses.txt'
-];
-var colValue = 'v';
+const URL = 'https://spdx.org/licenses/licenses.json';
+const MAX_CONCURRENT_CONNECTIONS = 10;
+
 var licensesJson = {};
+var counter = 0;
 
-// process the excel sheet and generate json files from it
-function processXls() {
-	cv2json.xls(globby.sync('licenses/spdx_licenselist_*.xls')[0], function (err, result) {
-		if (err) {
-			throw err;
-		}
-
-		var licenses = result.Sheets.licenses;
-		for (var key in licenses) {
-			var row = key.match(/^A(\d+)$/);
-			if (row && row[1] > 1) {
-				var name = licenses[row[0]][colValue].trim();
-				var identifier = licenses['B' + row[1]][colValue].trim();
-				var url = licenses['C' + row[1]] ? licenses['C' + row[1]][colValue] : null;
-				var approved = licenses['E' + row[1]] ? licenses['E' + row[1]][colValue] === 'YES' : false;
-
-				licensesJson[identifier] = {
-					name: name,
-					url: url,
-					osiApproved: approved
-				};
-			}
-		}
-
-		fs.writeFileSync('spdx.json', JSON.stringify(licensesJson, null, '\t'));
-
-		Object.keys(licensesJson).forEach(function (item) {
-			licensesJson[item].license = fs.readFileSync(path.join(folder, item + '.txt')).toString().trim();
+function downloadLicense(license, callback) {
+	got(license.detailsUrl, { json: true })
+		.then(response => {
+			var details = response.body;
+			licensesJson[license.licenseId].license = details.licenseText.replace(/\r\n/g, '\n').trim();
+			console.log(`Downloaded ${++counter} of ${Object.keys(licensesJson).length} license(s).`)
+			callback(null);
+		})
+		.catch(error => {
+			callback(`Error getting URL ${license.detailsUrl}. Response is:\n${error.response.body}`);
 		});
-
-		fs.writeFileSync('spdx-full.json', JSON.stringify(licensesJson, null, '\t'));
-
-		// clean the `licenses` folder
-		fs.readdir(folder, function (err, files) {
-			if (err) {
-				throw err;
-			}
-
-			files.forEach(function (file) {
-				if (!file.match(/\.txt$/)  || ignored.indexOf(file) !== -1 || /^deprecated_/.test(file)) {
-					fs.unlink(path.join(folder, file));
-				}
-			});
-		});
-	});
 }
 
-del.sync('licenses');
+got(URL, { json: true })
+	.then(response => {
+		var spdx = response.body;
 
-got(url)
-	.pipe(zlib.createGunzip())
-	.pipe(tar.Extract({ path: folder, strip: 1}))
-	.on('error', function (err) {
-		throw err;
+		for (const license of spdx.licenses) {
+			licensesJson[license.licenseId] = {
+				name: license.name,
+				url: license.seeAlso[0], // Only get the first of possibly multiple URLs.
+				osiApproved: license.isOsiApproved
+			};
+		}
+
+		// Hash of hashes that maps license names to license properties.
+		fs.writeFileSync('spdx.json', JSON.stringify(licensesJson, null, '\t'));
+
+        // Simple array of license names.
+		fs.writeFileSync('spdx-simple.json', JSON.stringify(Object.keys(licensesJson), null, '\t'));
+
+		var q = d3.queue(MAX_CONCURRENT_CONNECTIONS);
+		for (const license of spdx.licenses) {
+			q.defer(downloadLicense, license);
+		}
+		q.awaitAll((error, terms) => {
+			if (error) {
+				console.log(error);
+			} else {
+				// Hash of hashes that maps license names to license properties
+                // including the full license text.
+				fs.writeFileSync('spdx-full.json', JSON.stringify(licensesJson, null, '\t'));
+			}
+		});
 	})
-	.on('end', processXls);
+	.catch(error => {
+		console.log(`Error getting URL ${url}. Response is:\n${error.response.body}`);
+	});
