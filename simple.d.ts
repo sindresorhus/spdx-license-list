dd25e6070db6982024b5087e111db8fd4fdc59d0
Sindre Sorhus <sindresorhus@gmail.com>
2020-09-17 01:46:00 +0200

    Update simple.d.ts

--- simple.d.ts
+++ simple.d.ts
@@ -3,11 +3,12 @@ Set of the identifiers of [SPDX licenses](https://spdx.org/licenses/).
 
 @example
 ```
-import * as spdxSimple from "spdx-license-list/simple";
+import * as spdxSimple from 'spdx-license-list/simple';
+
 console.log(spdxSimple);
 //=> Set {'0BSD', 'AAL', …}
 ```
 */
-declare const SpdxLicenseListSimple: Readonly<Set<string>>;
+declare const spdxLicenseListSimple: Readonly<Set<string>>;
 
-export = SpdxLicenseListSimple;
+export = spdxLicenseListSimple;
